69cf98562906d41f047ce25eb675dae379cac350
Vasu Bhatia <vasubhatia@dldavppcbsei.com>
2024-05-19 12:13:03 +0530

    Change index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+// Webtech Project
 import * as cocoSsd from "@tensorflow-models/coco-ssd";
 import "@tensorflow/tfjs";
 import "./styles.css";
